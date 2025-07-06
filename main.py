8d4388363f58b1c3c045c8efe478a9b1e47fc412
ProgSaif <isaifulone@gmail.com>
2025-07-06 15:47:19 +0600

    Update main.py
    Line 203

--- main.py
+++ main.py
@@ -200,7 +200,7 @@ async def forward_message(event):
         cleaned_text = remove_forbidden_words(cleaned_text, forbidden_words)
 
         # Add bold formatted hashtags at the end
-        formatted_text = f"{cleaned_text}\n#BINANCE #RedPacket~Hub"
+        formatted_text = f"{cleaned_text}\n Reaction Please \n #Binance #RedPacketHub"
         
         for channel in target_channels:
             try:
