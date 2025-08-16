6b66612c7d9536ad6e92aa33631f7e29bb9d2d8b
ProgSaif <isaifulone@gmail.com>
2025-08-16 17:39:23 +0600

    Update main.py

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ last_forward_time = 0
 # ======================
 #  forbidden_words (forbidden_words remove from message before forward the message)
 # ======================
-forbidden_words = ['big', 'box', 'slot', 'square', 'thxbox', 'thx', 'angelia','fake','@']
+forbidden_words = ['big', 'box', 'slot', 'square' , '#Binance #RedPacket🧧']
 
 # ======================
 #  MESSAGE PROCESSING
@@ -116,7 +116,7 @@ def should_forward(message_text, has_media):
         return False
 
     valid_numbers = ['BTC', 'USDT', '012', '991', 'LN', '111', '888', 'SSS', '999', 'AAA', '123', 'LN', 'TS', '3D', '4D', 'LU', 'TS', '4H', '12', '007', '99', '05', '09', '63', '101', '100', '18', 'Q', 'SR', 'UU', '5500', '5555', '6000', '6666', '10000']
-    forbidden_terms = ['http', 't.me', '@', 'thx', 'thxbox', 'agelia_quiz_bot','fake','FOLLOW','BinRedBoxes','B I N R E D B O X E S','bot','Group','Mystery','invalid','channel']
+    forbidden_terms = ['http', 't.me', '@', 'thx', 'thxbox', 'angelia', 'agelia_quiz_bot','fake','FOLLOW','BinRedBoxes','B I N R E D B O X E S','bot','Group','Mystery','invalid','channel']
 
     contains_number = any(num in message_text for num in valid_numbers)
     contains_forbidden = any(term.lower() in message_text.lower() for term in forbidden_terms)
