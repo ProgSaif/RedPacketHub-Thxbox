c9f67c35263c1d7beca2a0e4ca01701531902210
ProgSaif <isaifulone@gmail.com>
2025-08-17 10:52:18 +0600

    Update main.py

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ last_forward_time = 0
 # ======================
 #  forbidden_words (forbidden_words remove from message before forward the message)
 # ======================
-forbidden_words = ['big', 'box', 'slot', 'square' , '#Binance #RedPacket']
+forbidden_words = ['big', 'box', 'slot', 'square']
 
 # ======================
 #  MESSAGE PROCESSING
